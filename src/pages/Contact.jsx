5831b9976ca858c7e067f539a939c3f2de203ac9
M-Juss <jusssayson@gmail.com>
2025-06-19 00:20:30 +0800

    Fixed responsive bugs

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -5,28 +5,39 @@ import Footer from '../components/Footer';
 
 const Contact = () => {
   return (
-    <div className='flex-col bg-white h-screen min-w-screen'>
-      <NavBar/>
+    <div className='flex flex-col min-h-screen bg-white'>
+      <NavBar />
 
-      <div className='flex md space-x-30 h-fit'>
-        <div>
-          <img src=".\image\MintZip-Design.png" alt="" className='size-170 w-200' />
+      <div className='flex flex-col md:flex-row items-center md:items-start justify-center px-6 md:px-20 py-12 gap-10'>
+        <div className='w-full md:w-1/2 flex justify-center'>
+          <img src="./image/MintZip-Design.png" alt="MintZip Design" className='size-100 md:w-80 object-cover' />
         </div>
-        <div className='flex-col my-auto'>
-          <h1 className='font-semibold text-4xl pb-9'>Contact Us</h1>
+
+        <div className='w-fit md:full md:w-1/2 px-auto'>
+          <h1 className='font-semibold text-4xl pb-6'>Contact Us</h1>
           <h2 className='text-md'>Explore the future with us.</h2>
-          <h2 className='text-sm pb-7'>Feel free to get in touch.</h2>
-          <h2 className='text-sm font-semibold pt-7 pb-4'>Name</h2>
-          <Input placeholder="Fullname"/>
-          <h2 className='text-sm font-semibold pt-7 pb-4'>Email</h2>
-          <Input placeholder="Email Address"/>
-          <h2 className='text-sm font-semibold pt-7 pb-4'>Message</h2>
-          <Input placeholder="How can we get better?"/>
+          <h2 className='text-sm pb-6'>Feel free to get in touch.</h2>
+
+          <div className='pt-4'>
+            <h2 className='text-sm font-semibold pb-2'>Name</h2>
+            <Input placeholder="Fullname" />
+          </div>
+
+          <div className='pt-4'>
+            <h2 className='text-sm font-semibold pb-2'>Email</h2>
+            <Input placeholder="Email Address" />
+          </div>
+
+          <div className='pt-4'>
+            <h2 className='text-sm font-semibold pb-2'>Message</h2>
+            <Input placeholder="How can we get better?" />
+          </div>
         </div>
       </div>
-    <Footer />
-    </div>
-  )
-}
 
-export default Contact
+      <Footer />
+    </div>
+  );
+};
+
+export default Contact;
