69b4e96bf5ef595a75d2fbcbba95943b3f75deb4
M-Juss <jusssayson@gmail.com>
2025-06-27 23:41:31 +0800

    Created Sign-up page

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,21 @@ import About from './pages/About';
 import Contact from './pages/Contact';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ScrollToTop from './components/function/ScrollToTop';
+import SignUp from './pages/SignUp';
 
 function App() {
 
   return (
-    <Router>
-      <ScrollToTop/>
-      <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/pricing" element={<Pricing />}/>
-        <Route path="/about" element={<About/>}/>
-        <Route path="/contact" element={<Contact/>} />
-      </Routes>
-    </Router>
+    // <Router>
+    //   <ScrollToTop/>
+    //   <Routes>
+    //     <Route path="/" element={<Home />}/>
+    //     <Route path="/pricing" element={<Pricing />}/>
+    //     <Route path="/about" element={<About/>}/>
+    //     <Route path="/contact" element={<Contact/>} />
+    //   </Routes>
+    // </Router>
+    <SignUp/>
   )
 }
 
---
+++ src/pages/SignUp.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const SignUp = () => {
+  return (
+    <div className='flex justify-center align-middle items-center h-screen max-w-screen bg-white'>
+        <div className='flex flex-col px-15 py-13 rounded-2xl shadow-2xl'>
+            <img src='./image/mainLogo.png' alt='MintZip-Logo' className='h-30 w-35 mx-auto' />
+            <h1 className='text-center font-semibold text-4xl pb-7 '>Sign Up</h1>
+            <input placeholder='Name' type='text' className='border-1 border-gray-l00 rounded-md outline-none focus:outline-none mb-5 px-3 py-2 text-l w-80'></input>
+            <input placeholder='Email' type='email' className='border-1 border-gray-l00 rounded-md outline-none focus:outline-none mb-5 px-3 py-2 text-l w-80'></input>
+            <input placeholder='Password' type='password' className='border-1 border-gray-l00 rounded-md outline-none focus:outline-none mb-5 px-3 py-2 text-l w-80'></input>
+
+            <button className='bg-blue-900 px-5 py-3 text-white hover:bg-blue-600 rounded-md'>Sign Up</button>
+            
+            <p className='text-gray-400 text-center pt-5'>Already have an account? <a href="#"className='text-black'>Log In</a></p>
+
+        </div>
+    </div>
+  )
+}
+
+export default SignUp
